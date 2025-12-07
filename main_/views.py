a62b1570af71afe62a579081e41afb2323acc928
Data <the_alchemist@fastmail.com>
2025-12-07 13:02:08 -0500

    Start to markdown html gen

--- main_/views.py
+++ main_/views.py
@@ -1,5 +1,28 @@
+from pathlib import Path
+
+import markdown
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+
+def compile_markdown() -> None:
+    """Compiles all relevant markdown files to HTML"""
+
+    md_files = [
+        "introduction",
+        "heathcare",
+        "election_reformeducation",
+        "veterans",
+        "infrastructure",
+        "healthcare",
+        "environment",
+    ]
+
+    for f in md_files:
+        md_text = Path(f"{f}.md").read_text(encoding="utf-8")
+        html = markdown.markdown(md_text, extensions=["extra", "toc", "codehilite"])
+
+        Path(f"{f}.html").write_text(html, encoding="utf-8")
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -18,9 +41,17 @@ def election_reform(request: HttpRequest) -> HttpResponse:
     return render(request, "election_reform.html", {})
 
 
+def environment(request: HttpRequest) -> HttpResponse:
+    return render(request, "environment.html", {})
+
+
 def housing(request: HttpRequest) -> HttpResponse:
     return render(request, "housing.html", {})
 
 
 def infrastructure(request: HttpRequest) -> HttpResponse:
     return render(request, "infrastructure.html", {})
+
+
+def veterans(request: HttpRequest) -> HttpResponse:
+    return render(request, "veterans.html", {})
