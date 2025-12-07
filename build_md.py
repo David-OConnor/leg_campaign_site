a538e462cdea07ebe447e9e4ef1c5b700b7e09d5
David O'Connor <the_alchemist@fastmail.com>
2025-12-07 14:57:12 -0500

    Style work on base

--- build_md.py
+++ build_md.py
@@ -4,6 +4,7 @@ import markdown
 
 from main_.views import pages
 
+
 def build_markdown() -> None:
     for page in pages:
         md_text = Path(f"./md_pages/{page}.md").read_text(encoding="utf-8")
@@ -17,9 +18,9 @@ def build_markdown() -> None:
 {{% endblock %}}
 """
 
-        print("TT: {:?}", template_text)
-
         Path(f"./templates/{page}.html").write_text(template_text, encoding="utf-8")
+
+    print("Complete; templates dir populated with HTML from MD files.")
 
 
 build_markdown()
--- main_/views.py
+++ main_/views.py
@@ -1,12 +1,10 @@
-from pathlib import Path
-
 from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render
 
 # Must match the markdown names. Used to create HTML files,
 # urls, and views.
 pages = [
-    # "introduction",
+    "index",
     "healthcare",
     "election_reform",
     "education",
@@ -16,54 +14,12 @@ pages = [
     "environment",
 ]
 
-# def build_markdown() -> None:
-#     for page in pages:
-#         md_text = Path(f"{page}.md").read_text(encoding="utf-8")
-#         html = markdown.markdown(md_text, extensions=["extra", "toc", "codehilite"])
-#
-#         template_text = f"""{{% extends "base.html" %}}
-# {{% load static %}}
-#
-# {{% block contents %}}
-# {html}
-# {{% endblock %}}
-# """
-#
-#         Path(f"templates/{page}.html").write_text(template_text, encoding="utf-8")
-
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "index_old.html", {})
+    return render(request, "index.html", {})
 
 
 def page_generic(request: HttpRequest, page: str) -> HttpResponse:
     if page not in pages:
         raise Http404()
     return render(request, f"{page}.html", {})
-
-# def bills(request: HttpRequest) -> HttpResponse:
-#     return render(request, "bills.html", {})
-#
-#
-# def education(request: HttpRequest) -> HttpResponse:
-#     return render(request, "education.html", {})
-#
-#
-# def election_reform(request: HttpRequest) -> HttpResponse:
-#     return render(request, "election_reform.html", {})
-#
-#
-# def environment(request: HttpRequest) -> HttpResponse:
-#     return render(request, "environment.html", {})
-#
-#
-# def housing(request: HttpRequest) -> HttpResponse:
-#     return render(request, "housing.html", {})
-#
-#
-# def infrastructure(request: HttpRequest) -> HttpResponse:
-#     return render(request, "infrastructure.html", {})
-#
-#
-# def veterans(request: HttpRequest) -> HttpResponse:
-#     return render(request, "veterans.html", {})
